8ca8236c487aa8b6a970018bb6f6db61cfa6f05b
NWarren42 <noahwarren42@gmail.com>
2024-11-09 09:52:21 -0500

    add file data extracting

--- qretproptools/gui/full_Gui/DataVisWidget.py
+++ qretproptools/gui/full_Gui/DataVisWidget.py
@@ -1,3 +1,4 @@
+import csv
 from typing import Any
 
 from PySide6.QtCore import Qt  #type:ignore
@@ -31,6 +32,7 @@ class DataVisWidget(QWidget):
         self.loadFileButton = QPushButton("Choose File")
         self.loadFileButton.clicked.connect(self.openFileDialog)
         self.fileLoadLayout.addWidget(self.loadFileButton, 1)
+        self.selectedFilePath = ""
 
         # Add a label to display the loaded file path
         self.filePathLabel = QLabel("No file loaded")
@@ -39,6 +41,13 @@ class DataVisWidget(QWidget):
         # Add the file load widget to the main layout
         self.mainLayout.addWidget(self.fileLoadWidget)
 
+        self.dataTimes: list[float] = []
+        self.sensorNames: list[str] = []
+        self.dataVals: dict[str, list[float]] = {}
+        self.configName = ""
+        self.configPath = ""
+        self.testTime = ""
+
     def openFileDialog(self) -> None:
         # Open file dialog and get selected file path
         file_path, _ = QFileDialog.getOpenFileName(self, "Open File", "", "All Files (*)")
@@ -46,3 +55,50 @@ class DataVisWidget(QWidget):
         # Update the label with the selected file path
         if file_path:
             self.filePathLabel.setText(file_path)
+            self.selectedFilePath = file_path
+            self.dataTimes, self.sensorNames, self.dataVals, self.configName, self.configPath, self.testTime = self.extractData(self.selectedFilePath)
+
+    def extractData(self,
+                    csvPath: str,
+                ) -> tuple[list[float],
+                           list[str],
+                           dict[str, list[float]],
+                           str,
+                           str,
+                           str,
+                           ]:
+        times = []
+        sensorData: dict[str, list[float]] = {}
+        sensorNames = []
+        configName = ""
+        configPath = ""
+        testTime = ""
+
+        with open(csvPath, "r") as csvFile:
+            csvReader = csv.reader(csvFile)
+            for row in csvReader:
+                if row[0] == "Config File Name:": # Grabbing config file name
+                    configName = row[1]
+
+                elif row[0] == "Config File Path:": # Acknowledging config path storage
+                    print(f"Config Path Found: {row[1]}")
+
+
+                elif row[0] == "Test Time:": # Grabbing time of the test
+                    testTime = row[1]
+
+                elif row[0] == "Time":
+                    for col in row:
+                        if col == "Time": continue # Ignoring time header for sensor list
+                        sensorData[col] = []
+                        sensorNames.append(col)
+
+                    print(f"{len(sensorNames)} sensors found: {sensorNames}")
+
+                else:
+                    times.append(float(row[0])) # Grabbing time values from first column
+                    for i, col in enumerate(sensorNames, start=1): # Using enumerate to address sensor names list as well as index the proper column
+                        # Data values are stored at specific column indices, but we want to store them to a named dictionary
+                        sensorData[col].append(float(row[i]))
+
+        return times, sensorNames, sensorData, configName, configPath, testTime
